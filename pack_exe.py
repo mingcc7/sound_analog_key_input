0e344f5de0665e601ffd6dbe189084384e605d5e
明c <13289105+mingccv@user.noreply.gitee.com>
2024-08-10 22:13:37 +0800

    更新

--- pack_exe.py
+++ pack_exe.py
@@ -12,6 +12,3 @@ PyInstaller.__main__.run([
 shutil.copy("configuration.json", "dist/run/configuration.json")
 shutil.copytree("language", "dist/run/language")
 shutil.copy("temp.wav", "dist/run/temp.wav")
-
-# pip install nuitka
-# nuitka --standalone run.py
